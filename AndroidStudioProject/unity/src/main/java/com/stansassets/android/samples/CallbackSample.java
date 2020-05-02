b25d6e8720599629f380492c44c4f64fa6d4abf9
Stanislav Osipov <lacost.st@gmail.com>
2020-05-03 01:12:11 +0300

    namespace change progres #1

---
+++ AndroidStudioProject/unity/src/main/java/com/stansassets/android/samples/CallbackSample.java
@@ -0,0 +1,21 @@
+package com.stansassets.android.samples;
+
+import com.stansassets.android.IUnityCallback;
+import com.stansassets.android.UnityBridge;
+
+public class CallbackSample {
+
+    public static class CallbackDataSample {
+        String m_StringData;
+        int m_IntData;
+    }
+
+    public static void ModelCallbackSample(final IUnityCallback callback) {
+        CallbackDataSample dataSample = new CallbackDataSample();
+
+        dataSample.m_StringData = "My String data";
+        dataSample.m_IntData = 100;
+
+        UnityBridge.sendCallback(callback, dataSample);
+    }
+}
---
+++ com.stansassets.android/Samples/CallbackSample.cs
@@ -0,0 +1,16 @@
+﻿using StansAssets.Android;
+using UnityEngine;
+
+
+public static class CallbackDataSample 
+{
+    String m_StringData;
+    int m_IntData;
+}
+
+public static class CallbackSample 
+{
+
+
+   
+}
